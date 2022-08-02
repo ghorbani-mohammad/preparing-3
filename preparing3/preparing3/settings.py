ffaaacd9ca5d9f3892e33fe56455adcc261cd187
Mohammad <aali361@gmail.com>
2022-08-02 21:49:39 +0430

    feat: add middleware

--- preparing3/preparing3/settings.py
+++ preparing3/preparing3/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'preparing3.middlewares.SimpleMiddleware'
 ]
 
 ROOT_URLCONF = 'preparing3.urls'
