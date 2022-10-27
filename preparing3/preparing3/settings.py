c67f568c1bf69f9b7a03794a2cb59b139caed4d8
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-27 08:08:07 +0330

    minor change

--- preparing3/preparing3/settings.py
+++ preparing3/preparing3/settings.py
@@ -47,9 +47,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'preparing3.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
